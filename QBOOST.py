6bec05c8f1f5f5203425bc9fefa82600dfb936b3
VZjunyang <vj.zhuang@gmail.com>
2023-06-12 15:39:36 +0200

    commit

--- QBOOST.py
+++ QBOOST.py
@@ -10,7 +10,7 @@ def Corr(l1,l2):
         Correlation function between two lists of the same length.
 
     ## Returns:
-        corr (float): correlation between the two lists
+        corr (float): correlation between the two lists.
 
     ## Parameters:
         l1 (list): list of predictions of the first classifier (1 or 0)
@@ -21,7 +21,7 @@ def Corr(l1,l2):
     # Get the correlation
     corr = 0
     for i in range(len(l1)):
-        corr += (2*l1[i]-1)*(2*l2[i]-1) # Scaling to -1,1
+        corr += (2*l1[i]-1)*(2*l2[i]-1)/N**2 # Scaling to -1/N,1/N
     return corr
 
 def QUBO(L,x,y,lambda_):
@@ -30,7 +30,7 @@ def QUBO(L,x,y,lambda_):
         Defines QUBO problem
 
     ## Returns:
-    Q: QUBO matrix
+        Q: QUBO matrix
 
 
     ## Parameters:
@@ -52,14 +52,14 @@ def QUBO(L,x,y,lambda_):
                 Q[i,j] = Corr(L[i].predict(x),L[j].predict(x))
                 Q[j,i] = Q[i,j]
             else:
-                Q[i,i] = lambda_ - 2*Corr(np.array(L[i].predict(x)), np.array(y)) + S/(N**2)
+                Q[i,i] = lambda_ - 2*Corr(np.array(L[i].predict(x)), np.array(y))
     
     return Q
 
 def simulated_annealing(func, init_state, T, reanneal, max_iter):
     """
         ## Description:
-            Simulated annealing algorithm
+            Simulated annealing algorithm.
             Finds the minimum of a function func using simulated annealing.
 
         ## Returns:
@@ -142,6 +142,18 @@ def RGS(L,x,y,lambda_):
     return w, energy
 
 def PredictRGS(L,w,x):
+    """
+    ## Description:
+        Predicts the labels of the data x using QBoost method (see paper for formula)
+
+    ## Returns:
+        List of predictions of the set of weak classifiers (0 or 1)
+
+    ## Parameters:
+        L (list): list of weak classifiers
+        w (array): solution of the QUBO problem
+        x (array): data
+    """
     
     N = len(L)
     S = len(x)
@@ -149,10 +161,62 @@ def PredictRGS(L,w,x):
     predictions = []
 
     for i in range(N):
-        predictions.append(L[i].predict(x))
+        predictions.append(2* L[i].predict(x) - 1 ) # Scaling to -1,1
 
     predictions = np.array(predictions)
-    predictions = 2*predictions - 1
+
+    # Calculate T
+    T = 0
+    for i in range(S):
+        for j in range(N):
+            T+= w[j]*predictions[j][i]
+    T = np.sign(T)
+
+    # Calculate predictions 
+    C = []
+    for i in range(S):
+        p = 0
+        for j in range(N):
+            p += w[j]*predictions[j][i]
+        p = np.sign(p-T)
+        C.append(p)
+    
+    # Replace -1 by 0
+    for i in range(len(C)):
+        if C[i] == -1:
+            C[i] = 0
+    return C
+
+def PredictRGS_probas(L,w,x,p):
+    """
+    ## Description:
+        Predicts the labels of the data x using QBoost method (see paper for formula) with a probability threshold
+
+    ## Returns:
+        List of predictions of the set of weak classifiers (0 or 1) using the threshold p
+
+    ## Parameters:
+        L (list): list of weak classifiers
+        w (array): solution of the QUBO problem
+        x (array): data
+        p (scalar): probability threshold bewteen 0 and 1
+    """
+
+    N = len(L)
+    S = len(x)
+    
+    predictions = []
+
+    for i in range(N):
+        predictions.append([])
+        proba = L[i].predict_proba(x)
+        for j in range(S):          
+            if proba[j][0] > p:
+                predictions[i].append(0)
+            else:
+                predictions[i].append(1)
+
+    predictions = np.array(predictions)
 
     # Calculate T
     T = 0
